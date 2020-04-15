265f7b24c81f716f1328a3dd4574f28bcc2d8445
Hector Prieto Moreno <hector.prieto.moreno@gmail.com>
2020-04-15 19:24:10 -0400

    feature: add sign up with password and email using firebase

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
               ...snapshot.data()
             }
           });
+          console.log(this.state.currentUser);
         })
       }
       this.setState({ currentUser: userAuth});
