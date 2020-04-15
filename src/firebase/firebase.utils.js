a8c10b838a7ba86eb54dfd1cbceffcab99ad829b
Hector Prieto Moreno <hector.prieto.moreno@gmail.com>
2020-04-15 18:43:23 -0400

    feature: add firebase store for storing users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
     appId: "1:95386071176:web:5d6fa858bc80b7d3a66f3d",
     measurementId: "G-6FM1FBZDSR"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
